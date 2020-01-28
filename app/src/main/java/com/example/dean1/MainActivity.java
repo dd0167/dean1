524b9cb227339f4b2af652df94074bcfd260b3ae
deang <dd0167@bs.amalnet.k12.il>
2020-01-28 21:47:58 +0200

    dean1-update

--- app/src/main/java/com/example/dean1/MainActivity.java
+++ app/src/main/java/com/example/dean1/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.dean1;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import static android.widget.Toast.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=(Button) findViewById(R.id.btn);
+    }
 
+    public void hello(View view) {
 
-
+        btn.setText("hello my name is Dean David");
     }
 }

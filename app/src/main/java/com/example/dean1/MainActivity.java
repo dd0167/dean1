6e97ef9f3e0070aa722f79dfb7bb1496f08a58c7
deang <dd0167@bs.amalnet.k12.il>
2020-01-28 09:53:55 +0200

    dean1-update

---
+++ app/src/main/java/com/example/dean1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.dean1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
